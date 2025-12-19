6de5e18f24ade1bcd1c0809204f54e2f0941fc28
Biswarup Biswas <117425933+biswa2005@users.noreply.github.com>
2025-12-19 20:27:27 +0530

    Ownership and borrower/references

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,49 @@ fn main() {
     let _s: &str = "Rup"; // String
     let _is_age: i32 = 18;
 
-    print!("Hello, world!");
-    print!("\nx : {}, y : {}, z : {}", _x, _y, _z);
-    print!("\nName : {}", _s);
+    println!("Hello, world!");
+    println!("x : {}, y : {}, z : {}", _x, _y, _z);
+    println!("Name : {}", _s);
 
-    // If-Else Condition 
+    // If-Else Condition
     // Turnary operators
     if _is_male && _is_age < 18 {
-        print!("\nYou are a male")
-    }
-    else if _is_male && _is_age >= 18 {
-        print!("\nYou are an adult male");
-    }
-    else{
-        print!("\nYou are a female");
+        println!("You are a male")
+    } else if _is_male && _is_age >= 18 {
+        println!("You are an adult male");
+    } else {
+        println!("You are a female");
     }
 
     let sentence = String::from("Your name is Rup");
-    print!("\nFirst Word : {}\n", get_first_word(sentence))
+    println!("First Word : {}", get_first_word(sentence));
+    // println!("{}", sentence) // The ownership of this sentence string is removed as it passes through the function
+
+    // Borrowers & References
+    let word1 = String::from("I Love Radha Krishna");
+    let word2  = get_first_word_new(&word1);
+    println!("{}", word2);
+    println!("{}", word1); // This can be done because word1 is passed through reference or the word2 borrow the value of word1 so the ownership of word1 is not changed ...
+    // We can also do word1.clone() but that operation is expensive as it makes an additional pointer and points at a newly made word1 string in heap but in this case word2 makes an additional pointer but points as the same as word1 address in heap. 
 }
 
+
 // Functions
-fn get_first_word(sentence : String) -> String {
-    let mut ans = String::new();
+fn get_first_word(sentence: String) -> String {
+    let mut ans = String::new(); // mut => mutable(the value can be changed)
+
+    // For Loop
+    for char in sentence.chars() {
+        ans.push(char);
+        if char == ' ' {
+            break;
+        }
+    }
+    return ans;
+}
+// Functions
+fn get_first_word_new(sentence: &String) -> String {
+    let mut ans = String::new(); // mut => mutable(the value can be changed)
 
     // For Loop
     for char in sentence.chars() {
